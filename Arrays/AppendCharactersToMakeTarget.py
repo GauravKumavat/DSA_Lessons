97541a80f7bfab3c1845b6a01caf07c53098b451
GauravScala <gkumar1005@gmail.com>
2025-08-28 11:49:58 +1000

    Append Character to Make Source Target string a subsequence of Source string

---
+++ Arrays/AppendCharactersToMakeTarget.py
@@ -0,0 +1,14 @@
+class AppendCharactersToMakeTarget:
+    def appendCharacter(self,s: str,t: str):
+        i,j = 0,0
+
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+                j += 1
+            else:
+                i +=1
+        return len(t) - j
+
+appendChar = AppendCharactersToMakeTarget()
+print(appendChar.appendCharacter("ABCDE","ADEFGH"))
